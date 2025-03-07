00de6d944488a973e9ee84799d904a684a47a300
Salvador <ssandovalg2100@alumno.ipn.mx>
2025-03-07 03:03:55 -0600

    Add files via upload

--- Base de datos/scripts/sismos_v0.0.1.sql
+++ Base de datos/scripts/sismos_v0.0.1.sql
@@ -1,12 +1,15 @@
 DROP DATABASE IF EXISTS "sismos";
 CREATE DATABASE "sismos";
 
-BEGIN
+BEGIN;
 \c sismos;
-CREATE EXTENSION postgis;
-\i registros-sismos.sql
-\i placas-tectonicas.sql
+\i placas_tectonicas.sql
+\i registros_sismos.sql
+COMMIT;
 
+BEGIN;
+\c sismos;
+\dt registros_sismos
 UPDATE registros_sismos AS rs
 SET placa_id = pt.id
 FROM placas_tectonicas AS pt
