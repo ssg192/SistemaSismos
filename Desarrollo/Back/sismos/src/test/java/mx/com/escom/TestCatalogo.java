dcfefb517245e92354dc3087b3871aad36ab49de
Gerardo Hernandez <gerahdez1807@gmail.com>
2025-06-25 02:02:46 -0600

    feat:PruebasUnitarias

---
+++ Desarrollo/Back/sismos/src/test/java/mx/com/escom/TestCatalogo.java
@@ -0,0 +1,49 @@
+package mx.com.escom;
+
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import mx.com.escom.sismos.external.rest.controller.CatalogoController;
+import mx.com.escom.sismos.external.rest.controller.SismoController;
+import org.jboss.resteasy.reactive.ResponseStatus;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@QuarkusTest
+@TestHTTPEndpoint(CatalogoController.class)
+
+public class TestCatalogo {
+    private static final String PATH_OBTAIN_PLACAS = "/placas";
+    private static final String PATH_OBTAIN_VOLCANES = "/volcanes";
+    @Test
+    void ObtenerPlacas(){
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .when()
+                .get(PATH_OBTAIN_PLACAS)
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+    }
+
+    @Test
+    void ObtenerVolcanes(){
+        given()
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+                .when()
+                .get(PATH_OBTAIN_VOLCANES)
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
+
+    }
+
+
+}
+
+
+
+
