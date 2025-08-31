c7cb293beb3d8965182761df84858c0c7a2e9849
Salvador Sandoval Garibay <emeth.salvador@ceneval.edu.mx>
2025-08-30 20:33:06 -0600

    feat: prueba bot6

--- Desarrollo/Back/sismos/src/main/java/mx/com/escom/sismos/core/entity/Sensores.java
+++ Desarrollo/Back/sismos/src/main/java/mx/com/escom/sismos/core/entity/Sensores.java
@@ -19,5 +19,5 @@ public class Sensores {
     private BigDecimal latitud;
     private BigDecimal longitud;
     private String red;
-    private String nombrePruebas4;
+    private String nombrePruebas5;
 }
